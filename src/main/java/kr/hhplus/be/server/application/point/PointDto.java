51c828a6b922b9267e5242449cf6b7c92fe7079b
donghass <ehdgk1221@gmail.com>
2025-04-11 00:41:18 +0900

    application Result

--- src/main/java/kr/hhplus/be/server/application/point/PointDto.java
+++
@@ -1,4 +0,0 @@
-package kr.hhplus.be.server.application.point;
-
-
-public record PointDto(Long userId, Long balance) { }
--- src/main/java/kr/hhplus/be/server/application/product/ProductDto.java
+++
@@ -1,3 +0,0 @@
-package kr.hhplus.be.server.application.product;
-
-public record ProductDto(Long id, String name, Long price, Long stock) { }
